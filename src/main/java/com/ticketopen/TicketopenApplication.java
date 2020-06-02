c7706d1803a1743e18f96d7c5d9463f512c1d696
Matheus da Silva Santos <matheus_silva521@hotmail.com>
2020-06-02 20:47:11 -0300

    first commit

---
+++ src/main/java/com/ticketopen/TicketopenApplication.java
@@ -0,0 +1,13 @@
+package com.ticketopen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketopenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketopenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketopen/TicketopenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketopen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketopenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
