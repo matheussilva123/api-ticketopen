3367e81277cb09ed4005a18a91e3ff887e8afa7b
Matheus da Silva Santos <matheus_silva521@hotmail.com>
2020-06-13 11:55:11 -0300

    creating instantiation operation

--- src/main/java/com/ticketopen/TicketopenApplication.java
+++ src/main/java/com/ticketopen/TicketopenApplication.java
@@ -1,13 +1,30 @@
 package com.ticketopen;
 
+import com.ticketopen.domain.Category;
+import com.ticketopen.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class TicketopenApplication {
+public class TicketopenApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketopenApplication.class, args);
-	}
+    @Autowired
+    private CategoryRepository categoryRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(TicketopenApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Printer");
+        Category cat2 = new Category(null, "Network");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
