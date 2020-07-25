156d8ca76d1281665203fd35b218ee4b51d9ce5e
Matheus da Silva Santos <matheus_silva521@hotmail.com>
2020-07-25 19:20:25 -0300

    translating the categories

--- src/main/java/com/ticketopen/TicketopenApplication.java
+++ src/main/java/com/ticketopen/TicketopenApplication.java
@@ -36,50 +36,57 @@ public class TicketopenApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Department dep1 = new Department(null, "Technology");
-        Department dep2 = new Department(null, "Financial");
-        Department dep3 = new Department(null, "Marketing");
-        Department dep4 = new Department(null, "Cleaning");
-        Department dep5 = new Department(null, "Human Resources");
-        Department dep6 = new Department(null, "Administration");
-        Department dep7 = new Department(null, "Management");
-        Department dep8 = new Department(null, "Maintenance");
+        Department dep1 = new Department(null, "Tecnologia");
+        Department dep2 = new Department(null, "Administração");
 
-        Category cat1 = new Category(null, "Printer", dep1);
-        Category cat2 = new Category(null, "Network", dep1);
-        Category cat3 = new Category(null, "Expenses refund", dep2);
-        Category cat4 = new Category(null, "Invoice issuance", dep2);
+        Category cat1 = new Category(null, "Impressora", dep1);
+        Category cat2 = new Category(null, "Rede", dep1);
+        Category cat3 = new Category(null, "Manutenção na máquina", dep1);
+        Category cat4 = new Category(null, "Limpeza na máquina", dep1);
+        Category cat5 = new Category(null, "Criar usuário no Ticket Open", dep1);
+        Category cat6 = new Category(null, "Restaurar dados perdidos", dep1);
+        Category cat7 = new Category(null, "Limpar disco rígido", dep1);
 
-        dep1.getCategoryList().addAll(Arrays.asList(cat1, cat2));
-        dep2.getCategoryList().addAll(Arrays.asList(cat3, cat4));
+        Category cat8 = new Category(null, "Cotação de novos serviços", dep2);
+        Category cat9 = new Category(null, "Manutenção nas estruturas", dep2);
+        Category cat10 = new Category(null, "Emissão de novo contrato", dep2);
+        Category cat11 = new Category(null, "Reembolso de Despesas", dep2);
+        Category cat12 = new Category(null, "Compras para o prédio", dep2);
+        Category cat13 = new Category(null, "Solicitação de uber", dep2);
+        Category cat14 = new Category(null, "Contatar motoboy para serviços externos", dep2);
+
+
+        dep1.getCategoryList().addAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+        dep2.getCategoryList().addAll(Arrays.asList(cat8, cat9, cat10, cat11, cat12, cat13, cat14));
 
         Person person1 = new Person(null, "Matheus da Silva Santos",
-                "matheus.dias@ifood.com.br", dep2);
+                "matheus_silva521@hotmail.com", dep2);
         Person person2 = new Person(null, "Rafael Araujo da Silva",
                 "rafaelajds@gmail.com", dep1);
 
         dep1.getPersonList().addAll(Arrays.asList(person1, person2));
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-        Ticket ticket1 = new Ticket(null, "Please check printer",
+        Ticket ticket1 = new Ticket(null, "A impressora não está imprimindo precisamos trocar o tonner",
                 sdf.parse("18/06/2020 16:00"),
-                sdf.parse("18/06/2020 19:52"), StateTicket.OPEN, cat1);
+                sdf.parse(""), StateTicket.OPEN, cat1);
 
         ticket1.getPersonList().addAll(Arrays.asList(person1, person2));
 
-        Ticket ticket2 = new Ticket(null, "Printer",
+        Ticket ticket2 = new Ticket(null, "Impressora está com defeito",
                 sdf.parse("02/01/2020 16:00"),
-                sdf.parse("06/01/2020 19:52"), StateTicket.OPEN, cat1);
+                sdf.parse(""), StateTicket.OPEN, cat1);
 
         ticket2.getPersonList().addAll(Arrays.asList(person1, person2));
 
-        Comment comment1 = new Comment(null, "The printer is normal...", person1, ticket1);
+        Comment comment1 = new Comment(null, "A impressora voltou a funcionar", person1, ticket1);
         ticket1.getCommentList().addAll(Arrays.asList(comment1));
 
 
-        departmentRepository.saveAll(Arrays.asList(dep1, dep2, dep3, dep4, dep5, dep6, dep7, dep8));
+        departmentRepository.saveAll(Arrays.asList(dep1, dep2));
         personRepository.saveAll(Arrays.asList(person1, person2));
-        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7,
+                cat8, cat9, cat10, cat11, cat12, cat13, cat14));
         ticketRepository.saveAll(Arrays.asList(ticket1, ticket2));
         commentRepository.saveAll(Arrays.asList(comment1));
     }
