edfe70f398a571c924e6fc87febdf859d480d912
Matheus da Silva Santos <matheus_silva521@hotmail.com>
2020-07-24 21:27:55 -0300

    add transactional

--- src/main/java/com/ticketopen/services/DepartmentService.java
+++ src/main/java/com/ticketopen/services/DepartmentService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,6 +30,7 @@ public class DepartmentService {
                         ", type class: " + Department.class.getName()));
     }
 
+    @Transactional
     public Department insertDepartment(Department obj) {
         obj.setId(null);
         return repo.save(obj);
