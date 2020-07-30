3f111006b8c9a2e3dc7adde9c47e220d1c30d97d
Matheus da Silva Santos <matheus_silva521@hotmail.com>
2020-07-29 22:30:52 -0300

    add JsonIgnore in personList

--- src/main/java/com/ticketopen/domain/Department.java
+++ src/main/java/com/ticketopen/domain/Department.java
@@ -1,5 +1,7 @@
 package com.ticketopen.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -18,6 +20,7 @@ public class Department implements Serializable {
     @OneToMany(mappedBy = "department", cascade= CascadeType.ALL)
     private List<Category> categoryList = new ArrayList<>();
 
+    @JsonIgnore
     @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
     private List<Person> personList = new ArrayList<>();
 
